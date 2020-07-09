14e963225d1ab4652439bfc3cf2b29d72f9f3a2e
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-09 13:29:39 -0300

    Cavalo e Rainha

--- src/chess/piece/Knight.java
+++ src/chess/piece/Knight.java
@@ -1,6 +1,7 @@
 package chess.piece;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,85 @@ public class Knight extends ChessPiece {
 	public String toString() {
 		return "C";
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// Acima 1 a Esquerda 2
+		p.setValues(position.getRow() - 1, position.getColumn() - 2);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Acima 1 a Direita 2
+		p.setValues(position.getRow() - 1, position.getColumn() + 2);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Acima 2 a Esquerda 1
+		p.setValues(position.getRow() - 2, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Acima 2 a Direita 1
+		p.setValues(position.getRow() - 2, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Abaixo 1 a Esquerda 2
+		p.setValues(position.getRow() + 1, position.getColumn() - 2);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Abaixo 1 a Direita 2
+		p.setValues(position.getRow() + 1, position.getColumn() + 2);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Abaixo 2 a Esquerda 1
+		p.setValues(position.getRow() + 2, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		// Abaixo 2 a Direita 1
+		p.setValues(position.getRow() + 2, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
 		return matriz;
 	}
 }
--- src/chess/piece/Queen.java
+++ src/chess/piece/Queen.java
@@ -1,6 +1,7 @@
 package chess.piece;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,11 +15,106 @@ public class Queen extends ChessPiece {
 	public String toString() {
 		return "Q";
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// Coluna Acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Coluna Abaixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Linha Esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Linha Direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Diagonal Acima a Esquerda
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			// p.setRow(p.getRow() - 1);
+			// p.setColumn(p.getColumn() - 1);
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Diagonal Abaixo a Esquerda
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Diagonal Acima A Direita
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			// p.setRow(p.getRow() - 1);
+			// p.setColumn(p.getColumn() + 1);
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Diagonal Abaixo A Direita
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+
 		return matriz;
 	}
-	
+
 }
